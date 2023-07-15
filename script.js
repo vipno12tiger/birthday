ed527e2d3471b75cc4e6ecd27667ccc54c015fe4
Vũ Hải Đăng <58963650+vipno12tiger@users.noreply.github.com>
2023-07-16 01:11:22 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector('.hearts-container');
-
-for (let i = 0; i < 10; i++) {
-  const heart = document.createElement('div');
-  heart.classList.add('heart');
-  heart.style.top = `${Math.random() * 100}%`;
-  heart.style.left = `${Math.random() * 100}%`;
-  container.appendChild(heart);
-}
